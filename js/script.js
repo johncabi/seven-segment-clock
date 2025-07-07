9f8179952434e246597873538e59644567e5fbec
John Lloyd Cabiling <johnlloydcabiling14@gmail.com>
2025-07-07 19:26:17 +0800

    chore: update script.js remove if-elses and convert clock elements to string, then add padstart to 2 0's

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let dayOfWeek, month, day, year, hours, minutes, seconds;
 
 dayOfWeek = document.getElementsByClassName("day");
-// console.log(dayOfWeek[0].outerText);
 month = document.getElementById("month");
 day = document.getElementById("day");
 year = document.getElementById("year");
@@ -45,50 +44,21 @@ setInterval(() => {
       break;
   }
 
+
   const dd = now.getDate();
   const m = now.getMonth();
   const yyyy = now.getFullYear();
+  const hh = now.getHours().toString().padStart(2, '0');
+  const mm = now.getMinutes().toString().padStart(2, '0');
+  const ss = now.getSeconds().toString().padStart(2, '0');
 
-  if (m < 10) {
-    month.innerText = `0${m}`;
-  } else {
-    month.innerText = m;
-  }
 
-  if (dd < 10) {
-    day.innerText = `0${dd}`;
-  } else {
-    month.innerText = dd;
-  }
-
-  if (m < 10) {
-    month.innerText = `0${m}`;
-  } else {
-    month.innerText = m;
-  }
-
+  month.innerText = m;
   day.innerText = dd;
   year.innerText = yyyy;
 
-  const hh = now.getHours();
-  const mm = now.getMinutes();
-  const ss = now.getSeconds();
 
-  if (hh < 10) {
-    hours.innerText = `0${hh}`;
-  } else {
-    hours.innerText = hh;
-  }
-
-  if (mm < 10) {
-    minutes.innerText = `0${mm}`;
-  } else {
-    minutes.innerText = mm;
-  }
-
-  if (ss < 10) {
-    seconds.innerText = `0${ss}`;
-  } else {
-    seconds.innerText = ss;
-  }
+  hours.innerText = hh;
+  minutes.innerText = mm;
+  seconds.innerText = ss;
 }, 500);
